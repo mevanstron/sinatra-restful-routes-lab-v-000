bdf660da0575cb182a56bd2c83097a476761e0e4
Michael Evans <michael@michaelgregoryevans.com>
2018-01-04 22:30:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     recipe.save
-    redirect '/recipes/<%= recipe.id %>'
+    redirect '/recipes/:id'
   end
 
   get '/recipes/:id' do
